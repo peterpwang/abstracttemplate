7d07afe6e90cc772045fc1adec075e7282825a3e
pw90 <peter.p.wang@outlook.com>
2020-09-20 19:48:02 +1200

    Changed output from single text file to randomly split train, validation and test text files.

--- html_process.py
+++ html_process.py
@@ -4,6 +4,7 @@
 import argparse
 import sys
 import os
+import random
 
 # public variables
 number_htmls = 0
@@ -14,7 +15,7 @@ def read_original_data(original_data_dir, text_data_path):
 
     global number_htmls, debug
 
-    f = open(text_data_path, 'w')
+    lines = []
 
     for subdir, dirs, files in os.walk(original_data_dir):
         for filename in files:
@@ -22,15 +23,34 @@ def read_original_data(original_data_dir, text_data_path):
 
             if filepath.endswith(".html"):
                 text = convert_html_to_text(filepath)
-                f.write(text);
-                f.write("\n");
+                lines.append(text)
 
                 number_htmls = number_htmls + 1
                 if debug == 1 and number_htmls%1000 == 0:
                     print(".", end = '', flush=True)
+
+    random.shuffle(lines)
+
+    number_train = int(number_htmls * 0.7)
+    number_validation = int(number_htmls * 0.2)
+    number_test = number_htmls - number_train - number_validation
+
+    f = open(text_data_path + "/train.txt", 'w')
+    for i in range(0, number_train):
+        f.write(lines[i] + "\n");
     f.close()
 
-    print(str(number_htmls) +  " converted.", flush=True)
+    f = open(text_data_path + "/validation.txt", 'w')
+    for i in range(number_train, number_train + number_validation):
+        f.write(lines[i] + "\n");
+    f.close()
+
+    f = open(text_data_path + "/test.txt", 'w')
+    for i in range(number_train + number_validation, number_htmls):
+        f.write(lines[i] + "\n");
+    f.close()
+
+    print(str(number_htmls) +  " (" + str(number_train) + "/" + str(number_validation) + "/" + str(number_test) + ") converted.", flush=True)
 
 
 def convert_html_to_text(html_path):
@@ -54,8 +74,8 @@ if __name__ == "__main__":
                         help='Debug?') 
     parser.add_argument('--input', default='./data/origin/', metavar='N',
                         help='Root path of HTML data files') 
-    parser.add_argument('--output', default='./data/text/data.txt', metavar='N',
-                        help='Output text data file path') 
+    parser.add_argument('--output', default='./data/text/', metavar='N',
+                        help='Output path of text data') 
     args = parser.parse_args()
 
     debug = args.debug
