bb11d4bf423db9e7431259cf9dea68dc65b4719a
pw90 <peter.p.wang@outlook.com>
2020-09-24 22:39:32 +1200

    Merged train.py and train_run.py

--- train.py
+++ train.py
@@ -6,6 +6,7 @@ import torch.nn.functional as F
 import os
 import time
 import math
+import argparse
 import logging
 import numpy as np
 import matplotlib.pyplot as plt
@@ -190,3 +191,16 @@ class Trainer():
             test_loss, math.exp(test_loss)))
         print('=' * 89)
 
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--epochs', default=1, type=int, metavar='N',
+                        help='number of total epochs to run (default 1)')
+    parser.add_argument('--batch_size', default=128, type=int, metavar='N',
+                        help='batch size (default 128)')
+    parser.add_argument('--data_path', default="./data/text/", type=str, metavar='N',
+                        help='data path (default ./data/text/)')
+    args = parser.parse_args()
+
+    net = Trainer(args)
+    net.run()
--- train.sh
+++ train.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 
 START_TIME=$SECONDS
-python3 train_run.py --epochs 2 --batch_size 32 --data_path=./data/text/
+python3 train.py --epochs 2 --batch_size 32 --data_path=./data/text/
 ELAPSED_TIME=$(($SECONDS - $START_TIME))
 echo " train.py took " $ELAPSED_TIME >> train_log.txt
 
