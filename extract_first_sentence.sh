721dc891b96d2eb78cef88ec73710d99b3ee1363
pw90 <peter.p.wang@outlook.com>
2020-10-06 21:38:48 +1300

    Removed useless files.

--- extract_first_sentence.sh
+++
@@ -1,6 +0,0 @@
-#/bin/bash
-
-awk -F '.' {'print $1"."'} data/2/train.txt > data/3/train.txt
-awk -F '.' {'print $1"."'} data/2/validation.txt > data/3/validation.txt
-awk -F '.' {'print $1"."'} data/2/test.txt > data/3/test.txt
-
