97f4d7dc7f179ae9008471238a01fbd1b1e22bbe
pw90 <peter.p.wang@outlook.com>
2020-10-07 22:33:12 +1300

    Added arguments of text generation script with the first word, output path, and number of created sentences.

--- generate.py
+++ generate.py
@@ -24,14 +24,16 @@ class Generator():
     def __init__(self, args):
 
         self.data_path = args.data_path
-        self.model_path = args.model_path
+        self.model_file = args.model_file
+        self.output_path = args.output_path
+        self.num_sentences = args.num_sentences
+        self.first_word = args.first_word
 
         self.corpus = None
 
         self.seed = 1111
         self.log_interval = 200
-        self.generated_path = "./data/9/generated.txt"
-        self.words = 100
+        self.words_per_sentence = 50
         self.temperature = 1.0
 
         # Set the random seed manually for reproducibility.
@@ -44,7 +46,7 @@ class Generator():
         print("Torch:", torch.__version__, "(CPU+GPU)" if  torch.cuda.is_available() else "(CPU)")
         torch.cuda.set_device(0)
 
-        with open(self.model_path, 'rb') as f:
+        with open(self.model_file, 'rb') as f:
             model = torch.load(f).to(device)
         model.eval()
 
@@ -53,23 +55,29 @@ class Generator():
         corpus = data.Corpus(self.data_path)
         ntokens = len(corpus.dictionary)
 
-        hidden = model.init_hidden(1)
-        input = torch.randint(ntokens, (1, 1), dtype=torch.long).to(device)
-
-        with open(self.generated_path, 'w') as outf:
+        with open(self.output_path + "/generated.txt", 'w') as outf:
             with torch.no_grad():  # no tracking history
-                for i in range(self.words):
-                    output, hidden = model(input, hidden)
-                    word_weights = output.squeeze().div(self.temperature).exp().cpu()
-                    word_idx = torch.multinomial(word_weights, 1)[0]
-                    input.fill_(word_idx)
+                for j in range(self.num_sentences):
+                    hidden = model.init_hidden(1)
+                    idx = corpus.dictionary.word2idx[self.first_word]
+                    input = torch.tensor([[idx]], dtype=torch.long).to(device)
+                    outf.write(self.first_word + ' ')
 
-                    word = corpus.dictionary.idx2word[word_idx]
+                    for i in range(self.words_per_sentence):
+                        output, hidden = model(input, hidden)
+                        word_weights = output.squeeze().div(self.temperature).exp().cpu()
+                        word_idx = torch.multinomial(word_weights, 1)[0]
+                        input.fill_(word_idx)
 
-                    outf.write(word + ('\n' if i % 20 == 19 else ' '))
+                        word = corpus.dictionary.idx2word[word_idx]
+                        outf.write(word + ' ')
+                        
+                        if word == '.':
+                            break;
 
-                    if i % self.log_interval == 0:
-                        print('| Generated {}/{} words'.format(i, self.words))
+                    outf.write('\n')
+
+        print("Text created.")
 
 
 if __name__ == "__main__":
@@ -77,8 +85,14 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--data_path', default="./data/3/", type=str, metavar='N',
                         help='data path (default ./data/3/)')
-    parser.add_argument('--model_path', default="./data/9/save.mdl", type=str, metavar='N',
+    parser.add_argument('--model_file', default="./data/9/save.mdl", type=str, metavar='N',
                         help='model path (default ./data/9/save.mdl)')
+    parser.add_argument('--output_path', default="./data/9/", type=str, metavar='N',
+                        help='output path (default ./data/9/)')
+    parser.add_argument('--num_sentences', default="10", type=int, metavar='N',
+                        help='Number of produced sentences (default 10)')
+    parser.add_argument('--first_word', default="This", type=str, metavar='N',
+                        help='First word to generate(default "This")')
     args = parser.parse_args()
 
     net = Generator(args)
--- generate.sh
+++ generate.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 
 START_TIME=$SECONDS
-python3 generate.py --data_path=./data/3/ --model_path=./data/9/save.mdl
+python3 generate.py --data_path=./data/3/ --model_file=./data/9/save.mdl --output_path=./data/9/ --num_sentences=10 --first_word=This
 ELAPSED_TIME=$(($SECONDS - $START_TIME))
 echo " generate.py took " $ELAPSED_TIME >> generate_log.txt
 
