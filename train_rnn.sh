b8e2f70c50f4aceb5e9ced860eb3064d5ad77c77
pw90 <peter.p.wang@outlook.com>
2021-02-05 22:35:34 +1300

    Renamed RNN scripts in order to remove them in the future

--- train_rnn.sh
+++ train_rnn.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 
 START_TIME=$SECONDS
-python3 train.py --epochs 50 --batch_size 32 --data_path=./data/5/ --output_path=./data/9/
+python3 train_rnn.py --epochs 50 --batch_size 32 --data_path=./data/5/ --output_path=./data/9/
 ELAPSED_TIME=$(($SECONDS - $START_TIME))
 echo " train.py took " $ELAPSED_TIME >> train_log.txt
 
