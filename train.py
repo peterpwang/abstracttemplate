4fa3d93665e41caf09c18b5f8216259652e3aa70
pw90 <peter.p.wang@outlook.com>
2021-02-08 22:07:07 +1300

    Changed epoch number in training

--- train.py
+++ train.py
@@ -175,7 +175,7 @@ def main():
     training_args = TrainingArguments(
         output_dir="./data/9/gpt2-theses", #The output directory
         overwrite_output_dir=True, #overwrite the content of the output directory
-        num_train_epochs=3, # number of training epochs
+        num_train_epochs=100, # number of training epochs
         per_device_train_batch_size=16, # batch size for training
         per_device_eval_batch_size=32,  # batch size for evaluation
         eval_steps = 400, # Number of update steps between two evaluations.
